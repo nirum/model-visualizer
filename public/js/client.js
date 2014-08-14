439fde79c533253b28ff8ac855004a9d106b03bd
Niru Maheswaranathan <nirum@stanford.edu>
2014-08-13 19:58:31 -0700

    added bootstrap styling to dashboard

--- public/js/client.js
+++ public/js/client.js
@@ -1,177 +1,78 @@
-App = Ember.Application.create();
+/* get dat */
+var update = function(expt) {
 
-App.Store = DS.Store.extend({
-  adapter: 'DS.FixtureAdapter'
-});
+  d3.json('/expt/' + expt, function(models) {
 
-/* ------
- * ROUTES
- * ------ */
-App.Router.map(function() {
-  this.route("index", { path: "/" })
-  this.route("home")
-  this.resource("models", function() {
-    this.route("model", { path: '/:model_id' });
+    // add data for each model
+    var fobj = new Array(models.length);
+    var cc   = new Array(models.length);
+    var nll  = new Array(models.length);
+    var lld  = new Array(models.length);
+
+    // save every
+    var save_every = 5;
+
+    for (var i = 0; i < models.length; i++) {
+
+      // select every nth element
+      var tempArray = []
+      for (var j = 0; j < models[i].fobj.length; j+=save_every) {
+        tempArray[j] = models[i].fobj[j];
+      }
+
+      fobj[i] = ["Cell " + models[i].cellidx].concat(tempArray);
+      cc[i]   = ["Cell " + models[i].cellidx].concat(models[i].corrcoef);
+      nll[i]  = ["Cell " + models[i].cellidx].concat(models[i].neg_loglikelihood);
+      lld[i]  = ["Cell " + models[i].cellidx].concat(models[i].loglikelihood_difference);
+
+      if (i == models.length-1) {
+        $("#update").prop("disabled", false);
+        $("#update").html("Update");
+      }
+
+    }
+
+    c3.generate({
+      bindto: '#objective',
+      data: {'columns': fobj},
+      axis: { 
+        y: { label: { text: 'Objective', position: 'outer-middle' } }
+      }
+    });
+
+    c3.generate({
+      bindto: '#cc',
+      data: {'columns': cc},
+      axis: { 
+        y: { label: { text: 'Corr. Coeff.', position: 'outer-middle' } }
+      }
+    });
+
+    c3.generate({
+      bindto: '#nll',
+      data: {'columns': nll},
+      axis: { 
+        y: { label: { text: 'Neg. Log-likelihood', position: 'outer-middle' } }
+      }
+    });
+
+    c3.generate({
+      bindto: '#lld',
+      data: {'columns': lld},
+      axis: { 
+        y: { label: { text: 'Log-likelihood improvement', position: 'outer-middle' } }
+      }
+    });
+
   });
+};
+
+// event listeners
+$("#update").click(function() {
+
+  // show loading bar, hide update button
+  $("#update").prop("disabled", true);
+  $("#update").html("Loading...");
+  update($("#expt").val());
+
 });
-
-App.HomeRoute = Ember.Route.extend({
-	model: function() {
-		return {'foo': 'bar'}
-	}
-});
-
-App.ModelsIndexRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('models', App.Modelfit.find());
-  }
-});
-
-//App.ModelsModelRoute = Ember.Route.extend({
-  //setupController: function(controller) {
-    //controller.set('model', data['model_id']);
-  //}
-//});
-
-/* ------
- * MODELS
- * ------ */
-App.Modelfit = DS.Model.extend({
-  name: DS.attr('string'),
-  timestamp: DS.attr('string')
-})
-//App.Modelfit = DS.Model.extend({
-  //data: DS.hasMany('iter'),
-  //timestamp: DS.attr('string')
-//});
-
-App.Iterdata = DS.Model.extend({
-  modelfit: DS.belongsTo('modelfit'),
-  iter: DS.attr('number'),
-  fobj: DS.attr('number')
-})
-
-//App.Team.FIXTURES = [{
-  //id: 1,
-  //name: 'Celtics',
-  //colors: 'Green, White'
-//}, {
-  //id: 2,
-  //name: 'Lakers',
-  //colors: 'Yellow, Black'
-//}, {
-  //id: 3,
-  //name: 'Bulls',
-  //colors: 'Red, Black'
-//}, {
-  //id: 4,
-  //name: 'Mavericks',
-  //colors: 'Blue, White'
-//}, {
-  //id: 5,
-  //name: 'Spurs',
-  //colors: 'Black, Grey, White'
-//}];
-
-App.Modelfit.FIXTURES = [{
-  id: 1,
-  name: "apple",
-  obj: [{
-    id: 1,
-    iter: 5,
-    fobj: 4.242
-  }, {
-    id: 2,
-    iter: 10,
-    fobj: 3.142
-  }, {
-    id: 3,
-    iter: 15,
-    fobj: 2.983
-  }, {
-    id: 4,
-    iter: 20,
-    fobj: 2.851
-  }, {
-    id: 25,
-    iter: 5,
-    fobj: 2.843
-  }, {
-    id: 6,
-    iter: 30,
-    fobj: 2.811
-  }],
-  timestamp: "7:42pm June 6, 2014"
-}, {
-  id: 2,
-  name: "orange",
-  obj: [{
-    id: 1,
-    iter: 10,
-    fobj: 19.1012
-  }, {
-    id: 2,
-    iter: 20,
-    fobj: 16.9234
-  }, {
-    id: 3,
-    iter: 30,
-    fobj: 14.2459
-  }, {
-    id: 4,
-    iter: 40,
-    fobj: 13.5235
-  }],
-  timestamp: "9:13am July 14, 2014"
-}];
-
-//{
-  //"apple": {
-    //"data": [{
-      //'id': 1,
-      //'iter': 5,
-      //'fobj': 4.242
-    //}, {
-      //'id': 2,
-      //'iter': 10,
-      //'fobj': 3.142
-    //}, {
-      //'id': 3,
-      //'iter': 15,
-      //'fobj': 2.983
-    //}, {
-      //'id': 4,
-      //'iter': 20,
-      //'fobj': 2.851
-    //}, {
-      //'id': 25,
-      //'iter': 5,
-      //'fobj': 2.843
-    //}, {
-      //'id': 6,
-      //'iter': 30,
-      //'fobj': 2.811
-    //}],
-    //"timestamp": "7:42pm June 6, 2014"
-  //},
-  //"orange": {
-    //obj: [{
-      //'_id': 1,
-      //'iter': 10,
-      //'fobj': 19.1012
-    //}, {
-      //'_id': 2,
-      //'iter': 20,
-      //'fobj': 16.9234
-    //}, {
-      //'_id': 3,
-      //'iter': 30,
-      //'fobj': 14.2459
-    //}, {
-      //'_id': 4,
-      //'iter': 40,
-      //'fobj': 13.5235
-    //}],
-    //"timestamp": "9:13am July 14, 2014"
-  //}
-//}
